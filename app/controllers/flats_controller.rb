6a98c76e4c24acca63dc0dd345612b2928409b6a
andre ferrer <andrerferrer@hotmail.com>
2020-10-30 19:10:45 -0300

    yarn add mapbox-gl

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -5,6 +5,13 @@ class FlatsController < ApplicationController
   # GET /flats.json
   def index
     @flats = Flat.all
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+
   end
 
   # GET /flats/1
